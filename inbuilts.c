6bf0097c5b1a466640d446843a435dd48fd006c7
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-05-22 18:49:59 +0300

    Update inbuilts to handle env, setenv and unsetenv
    Change return value of -1 to -2, since env functions return -1 on
    failure.
    Add conditonals for env and set & unset env.

--- inbuilts.c
+++ inbuilts.c
@@ -7,19 +7,22 @@
  * @p: system paths
  * @t: input tokens
  * @a: current alias list
+ * @o_env_adrs: pointer to a pointer to the original environ.
+ * @o_env_elms: pointer to an array of original environ elements address.
  *
  * Return: 1 if not inbuild command, 0 otherwise
  */
-int handle_inbuilts(char *sh, char *l, char **p, char **t, Alias **a)
+int handle_inbuilts(char *sh, char *l, char **p, char **t, Alias **a,
+								char **o_env_adrs, char **o_env_elms)
 {
 	int status = 0;
 
 	if (!strcmp(t[0], "exit"))
 	{
 		if (t[1])
-			exit_shell(strdup(t[1]), l, t, p, a);
+			exit_shell(strdup(t[1]), l, t, p, a, o_env_adrs, o_env_elms);
 		else
-			exit_shell(NULL, l, t, p, a);
+			exit_shell(NULL, l, t, p, a, o_env_adrs, o_env_elms);
 	}
 	if (!strcmp(t[0], "cd"))
 	{
@@ -35,5 +38,17 @@ int handle_inbuilts(char *sh, char *l, char **p, char **t, Alias **a)
 			free(t);
 		return (status);
 	}
-	return (-1);
+	if (!strcmp(t[0], "env") && !t[1])
+	{
+		_env();
+		free(t);
+		return (0);
+	}
+	if (!(strcmp(t[0], "setenv")) || !(strcmp(t[0], "unsetenv")))
+	{
+		status = set_unset_env(t, o_env_elms);
+		free(t);
+		return (status);
+	}
+	return (-2);
 }
