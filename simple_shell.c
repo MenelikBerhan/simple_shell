867c13ea5bb65c555bea193410aa1b81ba006064
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-05-22 18:52:10 +0300

    Update simple_shell after adding envs and changes
    Add two variables to store initial address of environ and its elelments.
    Add free_environ at the end

--- simple_shell.c
+++ simple_shell.c
@@ -14,6 +14,12 @@ int main(int argc, char *argv[])
 	FILE *fp = stdin;
 	Alias *l_alias = NULL;
 	(void)argc;
+	char **o_env_adrs = NULL,  **o_env_elms = NULL;
+
+	o_env_adrs = environ;
+	o_env_elms = malloc(sizeof(char *) * (_get_null_index() + 1));
+	o_env_elms = memcpy(o_env_elms, environ,
+						(sizeof(char *) * (_get_null_index() + 1)));
 
 	signal(SIGINT, handleSigInt);
 
@@ -31,12 +37,14 @@ int main(int argc, char *argv[])
 		if (line[nread - 1] == 10)
 			line[nread - 1] = 0;
 		trim_in(line);
-		multi_comms(argv[0], line, &l_alias, paths);
+		multi_comms(argv[0], line, &l_alias, paths, **o_env_adrs,
+													**o_env_elms);
 		prompt();
 	}
 	free(line);
 	free(*paths);
 	free(paths);
 	free_alias(l_alias);
+	free_environ(-1, o_env_adrs, o_env_elms);
 	return (0);
 }
