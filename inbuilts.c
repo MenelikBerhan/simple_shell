e6b06ff9635de0d5f14d055ea37aeb9f8bc620da
Toby2507 <oluwatobisalau2000@gmail.com>
2023-05-24 17:54:09 +0000

    Remove uncessary NULL argument to exit_shell call

--- inbuilts.c
+++ inbuilts.c
@@ -32,7 +32,7 @@ int handle_inbuilts(int u_a, char *sh, char *l, char **p, char **t, Alias **a,
 
 	if (!strcmp(t[0], "exit"))
 	{
-			exit_shell(u_a, NULL, l, t, p, a, o_env_adrs, o_env_elms);
+			exit_shell(u_a, l, t, p, a, o_env_adrs, o_env_elms);
 	}
 	if (!strcmp(t[0], "cd"))
 	{
