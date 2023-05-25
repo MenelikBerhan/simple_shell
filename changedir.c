97f1eb137cd6160295471eb0d0999941f4d04cee
Toby2507 <oluwatobisalau2000@gmail.com>
2023-05-25 01:35:11 +0000

    Fix cd command related errors

--- changedir.c
+++ changedir.c
@@ -19,8 +19,25 @@ int cd_errors(char *sh, char *path)
 		fprintf(stderr, "cd: usage: cd [-L|-P] [dir]\n");
 	}
 	else
-		fprintf(stderr, "%s: cd: %s: No such file or directory\n", sh, path);
+		fprintf(stderr, "%s: 1: cd: can't cd to %s\n", sh, path);
 	return (-1);
+}
+
+/**
+ * set_envs - set the PWD and OLDPWD envs
+ * @oldpwd: oldpwd content
+ * @fullpwd: fullpwd content
+ * @o_env_elms: pointer to an array of original environ elements address.
+ *
+ * Return: 0 if successful and -1 otherwise
+ */
+int set_envs(char *oldpwd, char *fullpwd, char **o_env_elms)
+{
+	if (_setenv("OLDPWD", oldpwd, 1, o_env_elms))
+		return (cd_errors("setenv() error", NULL));
+	if (_setenv("PWD", fullpwd, 1, o_env_elms))
+		return (cd_errors("setenv() error", NULL));
+	return (0);
 }
 
 /**
@@ -33,17 +50,19 @@ int cd_errors(char *sh, char *path)
  */
 int changedir(char *sh, char **t, char **o_env_elms)
 {
-	static char oldpwd[PATH_MAX];
-	char currpwd[PATH_MAX], fullpwd[PATH_MAX], *home;
+	char currpwd[PATH_MAX], fullpwd[PATH_MAX], oldpwd[PATH_MAX] = "", *h, *o;
 
 	if (!(getcwd(currpwd, sizeof(currpwd))))
 		return (cd_errors(sh, NULL));
+	o = _getenv("OLDPWD");
+	if (o)
+		strncpy(oldpwd, o, sizeof(oldpwd) - 1);
 	if (t[1] == NULL)
 	{
-		home = _getenv("HOME");
-		if (!home)
+		h = _getenv("HOME");
+		if (!h)
 			return (1);
-		strncpy(fullpwd, home, sizeof(fullpwd) - 1);
+		strncpy(fullpwd, h, sizeof(fullpwd) - 1);
 		fullpwd[sizeof(fullpwd) - 1] = '\0';
 	}
 	else if (t[1][0] == '/')
@@ -53,8 +72,12 @@ int changedir(char *sh, char **t, char **o_env_elms)
 	}
 	else if (strcmp(t[1], "-") == 0)
 	{
-		strncpy(fullpwd, oldpwd, sizeof(fullpwd) - 1);
+		if (!(*oldpwd))
+			strncpy(fullpwd, currpwd, sizeof(fullpwd) - 1);
+		else
+			strncpy(fullpwd, oldpwd, sizeof(fullpwd) - 1);
 		fullpwd[sizeof(fullpwd) - 1] = '\0';
+		printf("%s\n", fullpwd);
 	}
 	else
 		snprintf(fullpwd, sizeof(fullpwd) + 2, "%s/%s", currpwd, t[1]);
@@ -62,10 +85,8 @@ int changedir(char *sh, char **t, char **o_env_elms)
 	{
 		strncpy(oldpwd, currpwd, sizeof(oldpwd) - 1);
 		oldpwd[sizeof(oldpwd) - 1] = '\0';
-		if (_setenv("PWD", fullpwd, 1, o_env_elms))
-			return (cd_errors("setenv() error", NULL));
+		return (set_envs(oldpwd, fullpwd, o_env_elms));
 	}
 	else
 		return (cd_errors(sh, t[1]));
-	return (0);
 }
